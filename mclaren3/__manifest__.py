f783d467962be60efd153bbae08c564de2aa78be
rcucho <38509900+rcucho@users.noreply.github.com>
2021-07-22 11:55:03 -0500

    Add files via upload

---
+++ mclaren3/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Jasatec - Mstech',
+    'version': '14.0.1.0.0',
+    'author': 'MSTECH',
+    'category' : 'Technical Configuration',
+    'website': 'http://www.mstech.pe',
+    'depends': [
+        'mstech',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'sequence': 1,
+}
